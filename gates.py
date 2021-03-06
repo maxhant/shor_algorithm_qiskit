2417902e83a8df68b9272836940d45bc7a888698
maxhant <52139175+maxhant@users.noreply.github.com>
2021-03-06 08:50:50 -0400

    Functions are commented but still a is a confusing bit

--- gates.py
+++ gates.py
@@ -2,32 +2,77 @@ import qiskit as q
 import numpy as np
 
 def Carry(c, idx, reverse = False):
+    """
+    Function: 
+    It does bit's addition by considering a previously carried bit.
+    It return a carry bit.
+    
+    Args:
+    c: circuit
+    idx: list of 4 qubits [carry_i, bit_1, bit_2, carry_(i+1)]
+    reverse (opt): bool to run the operation in reverse
+    
+    Return:
+    The circuit which carries intrisically the qubit's state. 
+    Carry_(i+1) carries the excess forward
+    """
     if not len(idx) == 4:
         raise Exception("Carry gate takes 4 qubits")
-    if reverse:
-        c.ccx(idx[0],idx[2],idx[3])
-        c.cx(idx[1],idx[2])
+    if not reverse:
         c.ccx(idx[1],idx[2],idx[3])
+        c.cx(idx[1],idx[2])
+        c.ccx(idx[0],idx[2],idx[3])
     else:
-        c.ccx(idx[1],idx[2],idx[3])
-        c.cx(idx[1],idx[2])
         c.ccx(idx[0],idx[2],idx[3])
+        c.cx(idx[1],idx[2])
+        c.ccx(idx[1],idx[2],idx[3])        
 #     c.barrier()
     return c
 
 def Sum(c, idx, reverse = False):
+    """
+    Function: 
+    It set the carry qubits back to 0
+    
+    Args:
+    c: circuit
+    idx: list of 3 qubits [carry_i, bit_1, bit_2]
+    reverse (opt): bool to run the operation in reverse
+    
+    Return:
+    The circuit which carries intrisically the qubit's state. 
+    Unchanged bit_1 and bit_2 but carry_i is set back to 0 if bit_1 and bit_2 
+    are ones.
+    """
     if not len(idx) == 3:
-        raise Exception("Carry gate takes 4 qubits")
-    if reverse:
-        c.cx(idx[0], idx[2])
-        c.cx(idx[1], idx[2])        
-    else:
+        raise Exception("Carry gate takes 4 qubits")        
+    if not reverse:
         c.cx(idx[1], idx[2])
         c.cx(idx[0], idx[2])
-#     c.barrier()
+    else:
+        c.cx(idx[0], idx[2])
+        c.cx(idx[1], idx[2])        
     return c
 
 def Adder(c, reg1, reg2, ancil_adder, reverse = False):
+    """
+    Function: 
+    It adds two numbers together. 
+    
+    Args:
+    c: circuit
+    reg1: one of the number added of length n
+    reg2: the other number of length n
+    ancil_adder: the n carrying qubits required
+    reverse (opt): bool to run the operation in reverse
+    
+    Return:
+    The circuit which carries intrisically the qubit's state. 
+    reg1 stays unchanged.
+    reg2 become reg1+reg2 or reg2-reg1 for reverse = False and True respectively
+    ancil_adder becomes completely 0 
+                except for its first qubit which indicates overflow
+    """
     n = len(reg1)
     try:
         ancil = ancil_adder[0:n+1]
@@ -35,16 +80,7 @@ def Adder(c, reg1, reg2, ancil_adder, reverse = False):
         raise Exception(f" {len(ancil_adder)} ancillaes when {n+1} required.")
     if n != len(reg2):
         raise Exception("Currently only supports addition of equal bit length")        
-    if reverse:
-        for i in range(1, n):
-            i = n - i
-            c = Sum(c, [ancil[i+1], reg1[i], reg2[i]], reverse=True)            
-            c = Carry(c, [ancil[i+1], reg1[i], reg2[i], ancil[i]], reverse=False)
-        c = Sum(c, [ancil[1], reg1[0], reg2[0]], reverse=True)
-        c.cx(reg1[0], reg2[0])
-        for i in range(n):
-            c = Carry(c, [ancil[i+1], reg1[i], reg2[i], ancil[i]], reverse=True)          
-    else:
+    if not reverse:
         for i in range(n):
             i = (n-1) - i
             c = Carry(c, [ancil[i+1], reg1[i], reg2[i], ancil[i]])
@@ -52,10 +88,41 @@ def Adder(c, reg1, reg2, ancil_adder, reverse = False):
         c = Sum(c, [ancil[1], reg1[0], reg2[0]])
         for i in range(1, n):
             c = Carry(c, [ancil[i+1], reg1[i], reg2[i], ancil[i]], reverse=True)
-            c = Sum(c, [ancil[i+1], reg1[i], reg2[i]])
+            c = Sum(c, [ancil[i+1], reg1[i], reg2[i]])        
+    else:
+        for i in range(1, n):
+            i = n - i
+            c = Sum(c, [ancil[i+1], reg1[i], reg2[i]], reverse=True)            
+            c = Carry(c, [ancil[i+1], reg1[i], reg2[i], ancil[i]], reverse=False)
+        c = Sum(c, [ancil[1], reg1[0], reg2[0]], reverse=True)
+        c.cx(reg1[0], reg2[0])
+        for i in range(n):
+            c = Carry(c, [ancil[i+1], reg1[i], reg2[i], ancil[i]], reverse=True) 
     return c
 
-def Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil_mod):
+def Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil_mod, reverse=False):
+    """
+    Function: 
+    It takes the modulo of two numbers' sum. 
+    
+    Args:
+    c: circuit
+    reg1: one of the number added of length n (reg1 > 0)
+    reg2: the other number of length n, note that reg2 < N
+    regN: register used to stored N used for the modulo
+    regN_ctrl: register used to copy N
+    ancil_mod: the n carrying qubits required and one extra controlled qubit 
+    reverse (opt): bool to run the operation in reverse
+    
+    Return:
+    The circuit which carries intrisically the qubit's state. 
+    reg1 stays unchanged.
+    reg2 becomes (reg1+reg2)%N for reverse = False 
+    reg2 becomes reg2-reg1 if reg2-reg1>N else N+reg2-reg1 for reverse = True
+    ancil_mod in fully set to 0
+    reg_N remains unchanged
+    reg_N_ctrl is set back to 0
+    """
     n = len(reg1)
     ancil = ancil_mod[0:n+1]
     if n != len(reg2):
@@ -64,62 +131,111 @@ def Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil_mod):
         ctrl = ancil_mod[n+1]
     except:
         raise Exception(f"Not enough ancilla with {len(ancil_mod)} given for {n+2} required")
-    c = Adder(c, reg1, reg2, ancil, reverse = False)
-    c.barrier()
-    c = Adder(c, regN, reg2, ancil, reverse = True)
-    c.barrier()
-    c.cx(ancil[0], ctrl)
-    c.barrier()
-    for i in range(n):
-        c.ccx(ctrl, regN[i], regN_ctrl[i])
-    c.barrier()
-    c = Adder(c, regN_ctrl, reg2, ancil, reverse = False)
-    c.barrier() 
-    #to restore regN_ctrl to 0
-    for i in range(n):
-        c.ccx(ctrl, regN[i], regN_ctrl[i])
-    c = Adder(c, reg1, reg2, ancil, reverse=True)
-    c.cx(ancil[0], ctrl)
-    c = Adder(c, reg1, reg2, ancil, reverse=False)
-    c.x(ctrl) #otherwise left to one
-
-    return c
-
-def Ctrl_zero(c, ctrl, regN, regN_ctrl):
-    n = len(regN)
-    if len(regN_ctrl) != n:
-        raise Exception(f'Need {n} ancillae to copy N')
-    # Ancils are all ones, then they are flipped if regN[i] is 1 so they become 
-    # exact opposite
-    for i in range(n):
-        c.ccx(ctrl, regN[i], regN_ctrl[i])
-    return c
     
-def Ctrl_mod(c, regx, reg1, reg2, regN, regN_ctrl, ancil_ctrl):
+    if not reverse:
+        c = Adder(c, reg1, reg2, ancil, reverse = False)
+        c.barrier()
+        c = Adder(c, regN, reg2, ancil, reverse = True)
+        c.barrier()
+        c.cx(ancil[0], ctrl)
+        c.barrier()
+        for i in range(n):
+            c.ccx(ctrl, regN[i], regN_ctrl[i])
+        c.barrier()
+        c = Adder(c, regN_ctrl, reg2, ancil, reverse = False)
+        c.barrier() 
+        #to restore regN_ctrl to 0
+        for i in range(n):
+            c.ccx(ctrl, regN[i], regN_ctrl[i])
+        c = Adder(c, reg1, reg2, ancil, reverse=True)
+        c.cx(ancil[0], ctrl)
+        c = Adder(c, reg1, reg2, ancil, reverse=False)
+        c.x(ctrl) #otherwise left to one
+    else:
+        c.x(ctrl) #otherwise left to one
+        c = Adder(c, reg1, reg2, ancil, reverse=True)
+        c.cx(ancil[0], ctrl)
+        c = Adder(c, reg1, reg2, ancil, reverse=False)
+        for i in range(n):
+            i = n - 1 - i
+            c.ccx(ctrl, regN[i], regN_ctrl[i])    
+        c = Adder(c, regN_ctrl, reg2, ancil, reverse = True)
+        for i in range(n):
+            i = n - 1 - i
+            c.ccx(ctrl, regN[i], regN_ctrl[i])
+        c.cx(ancil[0], ctrl)
+        c = Adder(c, regN, reg2, ancil, reverse = False)
+        c = Adder(c, reg1, reg2, ancil, reverse = True)
+    return c
+
+    
+def Mult_mod(c, regx, reg1, ctrl, reg2, regN, regN_ctrl, ancil_ctrl, reverse=False):
+    """
+    Function: 
+    It does ax mod n by doing \sum a x_{k}2^{k}.
+    If ctrl is |1> and |x_{k}> is |1>, the register inputed in the Adder_mod is x_{k} as |x_{k} x_{k} ... x_{k} * n> 
+    Args:
+    c: circuit
+    reg1: one of the number added of length n (reg1 > 0)
+    reg2: the other number of length n, note that reg2 < N
+    regN: register used to stored N used for the modulo
+    regN_ctrl: register used to copy N
+    ancil_mod: the n carrying qubits required and one extra controlled qubit 
+    reverse (opt): bool to run the operation in reverse
+    
+    Return:
+    The circuit which carries intrisically the qubit's state. 
+    reg1 stays unchanged.
+    reg2 becomes (reg1+reg2)%N for reverse = False 
+    reg2 becomes reg2-reg1 if reg2-reg1>N else N+reg2-reg1 for reverse = True
+    ancil_mod in fully set to 0
+    reg_N remains unchanged
+    reg_N_ctrl is set back to 0
+    """    
     n = len(regx)
-    ancil_mod = ancil_ctrl[0:n+2]
     if n != len(reg2):
         raise Exception("Currently only supports addition of equal bit length")
     try:
-        ctrl = ancil_ctrl[n+2]
+        ancil_mod = ancil_ctrl[0:n+2]
     except:
         raise Exception(f"Not enough ancilla with {len(ancil_ctrl)} given for {n+3} required")
-
-    for i in range(0, n):
-        ls = list(range(0, n))
-        ls.pop(i - 1)
-        for j in ls:
-            c.ccx(ctrl, regx[i], reg1[j])
-            c = Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil_mod)
-            c.ccx(ctrl, regx[i], reg1[j])
-    c.x(ctrl)
-    for i in range(n):
-        c.ccx(ctrl, regx[i], reg2[i])
-    c.x(ctrl)
+    if not reverse:
+        for i in range(0, n):
+            ls = list(range(0, n))
+            ls.pop(i - 1)
+            for j in ls:
+                c.ccx(ctrl, regx[i], reg1[j])
+                c = Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil_mod)
+                c.ccx(ctrl, regx[i], reg1[j])
+        c.x(ctrl)
+        for i in range(n):
+            c.ccx(ctrl, regx[i], reg2[i])
+        c.x(ctrl)        
+    else:
+        c.x(ctrl)
+        #x is dealt with from 0 to n in indexing, but everything is reversed. but not here
+        for i in range(n):
+            i = n - 1 - i
+            c.ccx(ctrl, regx[i], reg2[i])
+        c.x(ctrl)
+        for i in range(0, n):
+            i = n - 1 - i
+            ls = list(range(0, n))[::-1]
+            ls.pop(i - 1)
+            for j in ls:
+                c.ccx(ctrl, regx[i], reg1[j])
+                c = Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil_mod, reverse=True)
+                c.ccx(ctrl, regx[i], reg1[j])        
     return c
     
 
-    
+def Exp_mod(c, regx, reg1, regX, reg2, regN, regN_ctrl, ancil_ctrl):
+    n = len(regX)
+    c.x(regx)
+    for i in range(n):
+        c = Ctrl_mod(c, regx, reg1, regX[i], reg2, regN, regN_ctrl, ancil_ctrl, reverse=False)
+        c = Ctrl_mod(c, reg1, regx, regX[i], reg2, regN, regN_ctrl, ancil_ctrl, reverse=True)
+    return c
     
 
     
--- test_gates.py
+++ test_gates.py
@@ -1,26 +1,147 @@
 import qiskit as q
 import numpy as np
 from gates import *
-from handler import nb_to_reg, keys_to_nb
+from handler import nb_to_reg, keys_to_nb, size
 import pytest
 from qiskit import Aer
 backend = Aer.get_backend('qasm_simulator')
 
 def test_Adder():
-    for N in range(1, 8):
-        nb_max = int('1'*N, 2)
-        for n1 in range(1,int(nb_max/2)):
-            for n2 in range(1,int(nb_max/2)):
-                c = q.QuantumCircuit()
-                reg1, c = nb_to_reg(n1, c, name="a_{reg}", size=N)
-                reg2, c = nb_to_reg(n2, c, name="b_{reg}", size=N)
-                ancil = q.QuantumRegister(N+1, name='ancillae')
-                meas1 = q.ClassicalRegister(N, name='a_{meas}')
-                meas2 = q.ClassicalRegister(N, name='b_{meas}')
-                c.add_register(ancil, meas1, meas2)
+    n = 4
+    nb_max = int('1'*n, 2)
+    n1 = np.random.randint(1, int(nb_max/2))
+    n2 = np.random.randint(1, int(nb_max/2))
+    c = q.QuantumCircuit()
+    reg1, c = nb_to_reg(n1, c, name="a_{reg}", size=n)
+    reg2, c = nb_to_reg(n2, c, name="b_{reg}", size=n)
+    ancil = q.QuantumRegister(n+1, name='ancillae')
+    meas1 = q.ClassicalRegister(n, name='a_{meas}')
+    meas2 = q.ClassicalRegister(n, name='b_{meas}')
+    c.add_register(ancil, meas1, meas2)
 
-                c = Adder(c, reg1, reg2, ancil)
-                c.measure_all()
-                job = q.execute(c, backend=backend, shots=1)
-                out = job.result().get_counts(c)           
-                assert n1 + n2 == keys_to_nb(out, size=N)[1]
+    c = Adder(c, reg1, reg2, ancil)
+    c.measure_all()
+    job = q.execute(c, backend=backend, shots=1)
+    out = job.result().get_counts(c)
+    vals = keys_to_nb(out, size=n, nb_keys = 2)
+    assert n1 + n2 == vals[1]
+    assert n1 == vals[0]
+    assert n*'0' == vals[2][1:]
+    if n1+n2>=nb_max:
+        assert '1' == vals[2][0]
+    else:
+        assert '0' == vals[2][0]
+
+def test_Adder_r():
+    n = 4
+    nb_max = int('1'*n, 2)
+    n1, n2 = np.sort([np.random.randint(1, int(nb_max/2)), np.random.randint(1, int(nb_max/2))])
+    c = q.QuantumCircuit()
+    reg1, c = nb_to_reg(n1, c, name="a_{reg}", size=n)
+    reg2, c = nb_to_reg(n2, c, name="b_{reg}", size=n)
+    ancil = q.QuantumRegister(n+1, name='ancillae')
+    meas1 = q.ClassicalRegister(n, name='a_{meas}')
+    meas2 = q.ClassicalRegister(n, name='b_{meas}')
+    c.add_register(ancil, meas1, meas2)
+
+    c = Adder(c, reg1, reg2, ancil, reverse=True)
+    c.measure_all()
+    job = q.execute(c, backend=backend, shots=1)
+    out = job.result().get_counts(c)           
+    vals = keys_to_nb(out, size=n, nb_keys=2)
+    assert n2 - n1 == vals[1]
+    assert n1 == vals[0]
+    assert n*'0' == vals[2][1:]
+#     if n1+n2>=nb_max:
+#         assert '1' == vals[2][0]
+#     else:
+#         assert '0' == vals[2][0]    
+    
+def test_Adder_mod():
+    n = 4
+    nb_max = int('1'*n, 2)
+    n1, N, n = size([np.random.randint(1, int(nb_max/2)), np.random.randint(2, int(nb_max))])
+    n2 = np.random.randint(1, N)
+    c = q.QuantumCircuit()  
+    reg1, c = nb_to_reg(n1, c, name="a_{reg}", size=n)
+    reg2, c = nb_to_reg(n2, c, name="b_{reg}", size=n)
+    regN, c = nb_to_reg(N, c, name="capn_{reg}", size=n)
+    regN_ctrl = q.QuantumRegister(n, name='n_ctrl')
+    ancil = q.QuantumRegister(n+2, name='ancillae')
+    meas1 = q.ClassicalRegister(n, name='a_{meas}')
+    meas2 = q.ClassicalRegister(n, name='b_{meas}')
+    c.add_register(regN_ctrl, ancil, meas1, meas2)
+
+    c = Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil)
+    c.measure_all()
+    job = q.execute(c, backend=backend, shots=1)
+    out = job.result().get_counts(c)          
+    vals = keys_to_nb(out, size=n, nb_keys = 4)
+    assert (n2+n1)%N == vals[1]
+    assert n1 == vals[0]
+    assert N == vals[2]
+    assert 0 == vals[3]
+    assert (n+2)*'0' == vals[4]
+    
+def test_Adder_mod_r():
+    n = 4
+    nb_max = int('1'*n, 2)
+    n1, N, n = size([np.random.randint(1, int(nb_max/2)), np.random.randint(2, int(nb_max))])
+    n2 = np.random.randint(1, N)
+    c = q.QuantumCircuit()  
+    reg1, c = nb_to_reg(n1, c, name="a_{reg}", size=n)
+    reg2, c = nb_to_reg(n2, c, name="b_{reg}", size=n)
+    regN, c = nb_to_reg(N, c, name="capn_{reg}", size=n)
+    regN_ctrl = q.QuantumRegister(n, name='n_ctrl')
+    ancil = q.QuantumRegister(n+2, name='ancillae')
+    meas1 = q.ClassicalRegister(n, name='a_{meas}')
+    meas2 = q.ClassicalRegister(n, name='b_{meas}')
+    c.add_register(regN_ctrl, ancil, meas1, meas2)
+
+    c = Adder_mod(c, reg1, reg2, regN, regN_ctrl, ancil, reverse=True)
+    c.measure_all()
+    job = q.execute(c, backend=backend, shots=1)
+    out = job.result().get_counts(c)
+    vals = keys_to_nb(out, size=n, nb_keys=4)
+    if n2 - n1 != (n2-n1)%N:
+        assert n2-n1+N == vals[1] 
+    elif n2 - n1 < 0:
+        assert n1 - n2 == vals[1]
+    else:
+        assert n2 - n1 == vals[1]
+    assert n1 == vals[0]
+    assert N == vals[2]
+    assert 0 == vals[3]
+    assert (n+2)*'0' == vals[4]
+    
+def test_Mult_mod():
+    backend = Aer.get_backend('qasm_simulator')
+    c = q.QuantumCircuit()
+    """
+    x is the exponent and it is initialized with H gate, thus it covers all values from 0 to 2^{n} - 1
+    a is the guess done
+    N is the coprime number
+    """
+    x, N, n = size([3, 7])
+    regx, c = nb_to_reg(x, c, name="a_{reg}", size=n) #I dont think a is inputed here
+    reg2 = q.QuantumRegister(n, name='reg_0')
+    c.add_register(reg2)
+    reg1 = q.QuantumRegister(n, name='reg_1')
+    regN, c = nb_to_reg(N, c, name="capn_{reg}", size=n)
+    regN_ctrl = q.QuantumRegister(n, name='n_ctrl')
+    regX, c = nb_to_reg(1, c, name="x_{reg}", size=1)
+    ancil = q.QuantumRegister(n+2, name='ancillae')
+    meas1 = q.ClassicalRegister(n, name='a_{meas}')
+    meas2 = q.ClassicalRegister(n, name='b_{meas}')
+    c.add_register(reg1, regN_ctrl, ancil, meas1, meas2)
+
+    c = Mult_mod(c, regx, reg1, regX[0], reg2, regN, regN_ctrl, ancil, reverse=False)
+    c.measure_all()
+    job = q.execute(c, backend=backend, shots=1)
+    out = job.result().get_counts(c)
+    print(keys_to_nb(out, size=n, nb_keys=4))
+    print(out)
+    # print((a**x)%N)
+    print((x*7)%N)
+
+    assert True
